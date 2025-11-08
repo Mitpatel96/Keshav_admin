725c3a2af6dea38fda644767b7c05cc66096a8c1
MitPatel <mit.dobariya@webitoinfotech.com>
2025-11-08 19:47:40 +0530

    Update API base URL to point to the development server

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:8080/api'
+const API_BASE_URL = 'https://dev.api.keshavtraders.com.au/api'
 
 // Create axios instance
 const axiosInstance = axios.create({
